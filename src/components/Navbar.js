d86687b8f71521472498a8ec52a918e8d5768647
Devin <doliver@Devins-MBP.attlocal.net>
2019-09-27 17:35:14 -0500

    rename header navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -70,7 +70,7 @@ const Navbar = class extends React.Component {
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                AccountID Tracking
               </Link>
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
