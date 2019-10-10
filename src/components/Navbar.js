3df889c4a88556f5d7427c209147949bc5278c9d
Devin <doliver@Devins-MacBook-Pro.local>
2019-10-10 08:50:28 -0700

    updating image size

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Netlify" style={{ width: '75px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
